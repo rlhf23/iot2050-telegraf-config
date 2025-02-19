e3d57432b0819049b5f913844e0b5cac011438c9
user <user@name.com>
2025-02-19 20:37:46 +0200

    auto load

--- flake.nix
+++ flake.nix
@@ -27,16 +27,31 @@
             rustVersion
             rust-analyzer
             pkg-config
-            # Add any other dependencies you need for egui
             cmake
             fontconfig
+            nushell
+            # System libraries
+            clang
+            gtk3
+            libxkbcommon
+            openssl
             xorg.libX11
             xorg.libXcursor
             xorg.libXrandr
             xorg.libXi
             libGL
+            xorg.libxcb
+            xorg.libXfixes
+
+            wayland
+            libxkbcommon
           ];
           RUST_SRC_PATH = "${rustVersion}/lib/rustlib/src/rust/library";
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.libGL}/lib:$LD_LIBRARY_PATH
+            export LIBGL_DRIVERS_PATH=${pkgs.mesa.drivers}/lib/dri
+            exec nu
+          '';
         };
       }
     );
